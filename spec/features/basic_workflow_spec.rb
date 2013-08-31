629563e4f790a1d924d2b18c755222424119e9c6
David Edwards <david.edwr@gmail.com>
2013-08-31 18:51:26 -0400

    Feature spec to add asset after faction create

--- spec/features/basic_workflow_spec.rb
+++ spec/features/basic_workflow_spec.rb
@@ -10,15 +10,24 @@ feature "create faction:" do
   scenario "user creates faction" do
     visit factions_path
     click_on "create faction"
+
     expect(page).to have_content "create a faction"
 
     fill_in "Faction name", with: "House Dimir"
     fill_in "Player email", with: "darrinkatz@gmail.com"
     click_on "Create Faction"
+
     expect(page).to have_content "House Dimir"
     expect(page).to have_content "darrinkatz@gmail.com"
 
-    expect(page).to have_content "create asset"
+    click_on "create asset"
+    fill_in "Name", with: "Szadek"
+    fill_in "Value", with: 5
+    check "Covert"
+    click_on "Submit Build"
+
+    expect(page).to have_content "House Dimir"
+    expect(page).to have_content "5-Szadek (covert)"
   end
 
 end
