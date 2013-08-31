bb2432a121eaa28a1c4a845c3c0e98f5720aca8b
David Edwards <david.edwr@gmail.com>
2013-08-31 17:33:22 -0400

    Clean up format in basic_workflow_spec

--- spec/features/basic_workflow_spec.rb
+++ spec/features/basic_workflow_spec.rb
@@ -2,23 +2,23 @@ require "spec_helper"
 
 feature "create faction:" do
 
-	scenario "sees faction list" do
-		visit factions_path
-		expect(page).to have_content "factions"
-	end
+  scenario "sees faction list" do
+    visit factions_path
+    expect(page).to have_content "factions"
+  end
 
-	scenario "user creates faction" do
-		visit factions_path
-		click_on "create faction"
-		expect(page).to have_content "create a faction"
-		#save_and_open_page
-		fill_in "Faction name", with: "House Dimir"
-		fill_in "Player email", with: "darrinkatz@gmail.com"
-		click_on "Create Faction"
-		expect(page).to have_content "House Dimir"
-		expect(page).to have_content "darrinkatz@gmail.com"
+  scenario "user creates faction" do
+    visit factions_path
+    click_on "create faction"
+    expect(page).to have_content "create a faction"
 
-		expect(page).to have_content "create asset"
-	end
+    fill_in "Faction name", with: "House Dimir"
+    fill_in "Player email", with: "darrinkatz@gmail.com"
+    click_on "Create Faction"
+    expect(page).to have_content "House Dimir"
+    expect(page).to have_content "darrinkatz@gmail.com"
+
+    expect(page).to have_content "create asset"
+  end
 
 end
