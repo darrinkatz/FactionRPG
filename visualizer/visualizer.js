1ed671c2130f37a969a1e89beda5d8fbcb019afb
Darrin Katz <darrin.katz@shawmedia.ca>
2014-07-12 16:31:56 -0400

    adding visualizer using google realtime api and knockout

---
+++ visualizer/visualizer.js
@@ -0,0 +1,35 @@
+function extractAssets(turn)
+{
+	var turnObj = JSON.parse(turn);
+	var faction = turnObj["faction"];
+	var assets = turnObj["assets"];
+	
+	var result = JSON.parse(turn);
+	result["assets"] = new Array();
+	
+	for (i = 0; i < assets.length; i++)
+	{
+		if (turnObj["assets"][i]["faction"] == faction)
+		{
+			result["assets"].push(turnObj["assets"][i]);
+		}
+	}
+
+	return JSON.stringify(result);
+}
+
+function mapFromJsonToVisualizer(turn)
+{
+	var turnObj = JSON.parse(turn);
+	var assets = turnObj["assets"];
+
+	return JSON.stringify(assets);
+}
+
+function mapFromVisualizerToJson(visualizerData, turn)
+{
+	var turnObj = JSON.parse(turn);
+	turnObj["assets"] = JSON.parse(visualizerData);
+
+	return JSON.stringify(turnObj);
+}
