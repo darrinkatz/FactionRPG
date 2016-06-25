b4f8e1daa631ef0fd1284a98c58ddf592275dec5
Darrin Katz <darrin.katz@shawmedia.ca>
2016-06-25 19:39:38 -0400

    changes

--- effect_calculator/scripts.js
+++ effect_calculator/scripts.js
@@ -1,59 +1,84 @@
-var effectConditions;
+var data;
 
 $(document).ready(function(){
 	
-	$("select").change(updateResult);
-	$("input").change(updateResult);
+	$("select").change(updateOutcome);
+	$("input").change(updateOutcome);
 
-	effectConditions = loadConditions();
-	updateResult();
+	data = loadData();
+	updateOutcome();
 });
 
-function updateResult() {
+function updateOutcome() {
 	
-	var result = "";
+	var outcome = "";
 	
-	var effectValue = $( "#effect option:selected" ).val();
+	var checkValue = $( "#check option:selected" ).val();
 	var difficultyValue = $( "#difficulty" ).val();
-	var resistanceValue =  $( "#resistance" ).val();
+	var resultValue =  $( "#result" ).val();
 	
-	var difference = difficultyValue - resistanceValue;
+	var difference = resultValue - difficultyValue;
 	
-	if (difference <= -5) {
-		result += "Resisted!";
-		$( "#result" ).removeClass().addClass("success");
-	} else {
-		result += "Injury";
-		$( "#result" ).removeClass().addClass("fail_by_1");
+	if (difference >= 0) {
 		
-		if (difference > 0) {
-			result += ", "
+		var numSuccesses = Math.floor((difference + 5) / 5);
+		
+		$( "#outcome" ).removeClass().addClass("success");
+		
+		if (checkValue == "attack") {
+			if (numSuccesses == 1) {
+				outcome += data["attack"][0];
+			} else if (numSuccesses == 2) {
+				outcome += data["attack"][1];
+			} else {
+				outcome += data["attack"][2];
+			}
+		} else if (checkValue == "skill") {
+			outcome += numSuccesses + " degrees of Success";
+		} else {
+			outcome += "Resisted";
 		}
 		
-		if (difference > 0 && difference <= 5){
-			result += effectConditions[effectValue][0];
-			$( "#result" ).removeClass().addClass("fail_by_2");
-		} else if (difference > 5 && difference <= 10) {
-			result += effectConditions[effectValue][1];	
-			$( "#result" ).removeClass().addClass("fail_by_3");			
-		} else if (difference > 10) {
-			result += effectConditions[effectValue][2];
-			$( "#result" ).removeClass().addClass("fail_by_4");
+	} else {
+		
+		var numFailures = Math.floor(difference / 5) * -1;
+
+		if (checkValue == "attack") {
+			outcome += "Miss";
+			$( "#outcome" ).removeClass().addClass("fail_by_4");
+		} else if (checkValue == "skill") {
+			outcome += numFailures + " degrees of Failure";
+			$( "#outcome" ).removeClass().addClass("fail_by_4");
+		} else {
+			if (difference < 0 && difference >= -5){
+				outcome += data[checkValue][0];
+				$( "#outcome" ).removeClass().addClass("fail_by_1");
+			} else if (difference < -5 && difference >= -10) {
+				outcome += data[checkValue][1];
+				$( "#outcome" ).removeClass().addClass("fail_by_2");
+			} else if (difference < -10 && difference >= -15) {
+				outcome += data[checkValue][2];	
+				$( "#outcome" ).removeClass().addClass("fail_by_3");			
+			} else if (difference < -15) {
+				outcome += data[checkValue][3];
+				$( "#outcome" ).removeClass().addClass("fail_by_4");
+			}
 		}
 	}
 	
-	$( "#result" ).html(result);
+	$( "#outcome" ).html(outcome);
 }
 
-function loadConditions() {
+function loadData() {
 	
-	var effectConditions = new Array();
+	var data = new Array();
 	
-	effectConditions["damage"] = new Array("Dazed", "Staggered", "Incapacitated");
-	effectConditions["charm"] = new Array("Entranced", "Compelled", "Controlled");
-	effectConditions["sleep"] = new Array("Fatigued", "Exhausted", "Asleep");
-	effectConditions["paralyze"] = new Array("Hindered", "Stunned", "Paralyzed");
-	effectConditions["weaken"] = new Array("Trait -2", "Trait -5", "Trait Useless");
+	data["attack"] = new Array("Hit", "Hit + Multiattack DC+2", "Hit + Multiattack DC+5");
+	data["damage"] = new Array("Injury", "Injury + Dazed", "Injury + Staggered", "Injury + Incapacitated");
+	data["charm"] = new Array("Injury", "Injury + Entranced", "Injury + Compelled", "Injury + Controlled");
+	data["sleep"] = new Array("Injury", "Injury + Fatigued", "Injury + Exhausted", "Injury + Asleep");
+	data["paralyze"] = new Array("Injury", "Injury + Hindered", "Injury + Stunned", "Injury + Paralyzed");
+	data["weaken"] = new Array("Injury", "Injury + Trait -2", "Injury + Trait -5", "Injury + Trait Fails");
 	
-	return effectConditions;
+	return data;
 }
